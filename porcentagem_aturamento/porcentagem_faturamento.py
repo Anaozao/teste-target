7f725b76389e53a9ba31233a4ef3c2f93a78c875
Tulio <tutyparis@gmail.com>
2025-01-13 12:42:03 -0300

    porcentagem

--- porcentagem_aturamento/porcentagem_faturamento.py
+++ porcentagem_aturamento/porcentagem_faturamento.py
@@ -6,3 +6,20 @@ def calculate_percentage():
     outros = 19849.53
 
     total_revenue = sum([sp, rj, mg, es, outros])
+
+    sp_percentage = (sp / total_revenue) * 100
+    rj_percentage = (rj / total_revenue) * 100
+    mg_percentage = (mg / total_revenue) * 100
+    es_percentage = (es / total_revenue) * 100
+    outros_percentage = (outros / total_revenue) * 100
+
+    return (
+        f"SP representa {sp_percentage:.2f}% do total"
+        f"RJ representa {rj_percentage:.2f}% do total"
+        f"MG representa {mg_percentage:.2f}% do total"
+        f"ES representa {es_percentage:.2f}% do total"
+        f"Outros representa {outros_percentage:.2f}% do total"
+    )
+
+
+print(calculate_percentage())
